6abec983cdf6b7b679145d924535966fe563db22
Mozrix <sonyblue41@gmail.com>
2026-04-12 18:54:25 +0700

    Tugas LKP 5

--- app/src/main/java/com/example/prakpm_2457052003/MainActivity.kt
+++ app/src/main/java/com/example/prakpm_2457052003/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.prakpm_2457052003
 
+import android.content.ClipData
+import android.media.RouteListingPreference
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -17,14 +21,26 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.mutableStateSetOf
@@ -33,10 +49,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
 import model.GameGear
 import model.Source
 import com.example.prakpm_2457052003.ui.theme.PrakPM_2457052003Theme
+import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,83 +72,151 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    var isFavorite by remember { mutableStateOf(false) }
-    val GameGear = Source.dummyGameGear[2]
-    val GameGear2 = Source.dummyGameGear[1]
-    val GameGear3 = Source.dummyGameGear[0]
+    val gameGearList = Source.dummyGameGear
 
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp)
+            .statusBarsPadding(),
+        contentPadding = PaddingValues(24.dp),
+        verticalArrangement = Arrangement.spacedBy(24.dp)
     ) {
-
-        Row() {
-            Box() {
-                Image(
-                    painter = painterResource(id = R.drawable.keyboard),
-                    contentDescription = "Gambar ${GameGear.nama}",
-                    modifier = Modifier.size(200.dp)
-                )
-                IconButton(
-                    onClick = { isFavorite = !isFavorite },
-                    modifier = Modifier.padding(8.dp).align(Alignment.BottomEnd)
-                ) { }
-                Icon(
-                    imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.Favorite,
-                    contentDescription = "Fav Icons",
-                    tint = if (isFavorite) Color.Red else Color.Blue
-                )
-            }
-            Column(modifier = Modifier.padding(top = 40.dp)) {
-                Text(text = "Nama: ${GameGear.nama}")
-                Text(text = "Deskripsi: ${GameGear.deskripsi}")
-                Text(text = "Harga: Rp ${GameGear.harga}")
-            }
-        }
-        Button(onClick = {
-            println("Uhuyyy")
-        } ) {
-            Text("Lihat List")
-        }
-
-        Row() {
-            Image(
-                painter = painterResource(id = R.drawable.headset),
-                contentDescription = "Gambar ${GameGear2.nama}",
-                modifier = Modifier.size(200.dp)
+        item {
+            Text(
+                text = "Rekomendasi Pro Player",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
             )
-            Column(modifier = Modifier.padding(top = 40.dp)) {
-                Text(text = "Nama: ${GameGear2.nama}")
-                Text(text = "Deskripsi: ${GameGear2.deskripsi}")
-                Text(text = "Harga: Rp ${GameGear2.harga}")
+            LazyRow(
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                items(Source.reccomendGear) { GameGear ->
+                    GameGearItems(gear = GameGear)
+                }
             }
-        }
-        Button(onClick = {
-            println("Uhuyyy")
-        } ) {
-            Text("Lihat List")
-        }
-
-        Row() {
-            Image(
-                painter = painterResource(id = R.drawable.mouse),
-                contentDescription = "Gambar ${GameGear3.nama}",
-                modifier = Modifier.size(200.dp)
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(
+                text = "Game Gear List",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(8.dp)
             )
-            Column(modifier = Modifier.padding(top = 40.dp)) {
-                Text(text = "Nama: ${GameGear3.nama}")
-                Text(text = "Deskripsi: ${GameGear3.deskripsi}")
-                Text(text = "Harga: Rp ${GameGear3.harga}")
-            }
         }
-        Button(onClick = {
-            println("Uhuyyy")
-        } ) {
-            Text("Lihat List")
+        items(Source.dummyGameGear){
+            GameGear ->
+            DetailScreen(gear = GameGear)
+        }
+    }
+
+}
+
+@Composable
+fun GameGearItems(gear: GameGear) {
+    Card(
+        modifier = Modifier.width(160.dp),
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column {
+            Image(
+                painter = painterResource(id = gear.imageRes),
+                contentDescription = gear.nama,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp),
+                contentScale = ContentScale.Crop
+            )
+            Column(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = gear.nama,
+                    style = MaterialTheme.typography.titleSmall,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1
+                )
+                Text(
+                    text = "Rp ${gear.harga}",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
         }
     }
 }
+
+@Composable
+fun DetailScreen(gear: GameGear){
+    var isFavorite by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        )
+    ) {
+        Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) { // Tambah padding agar konten tidak mepet ke tepi card
+            Box {
+                Image(
+                    painter = painterResource(id = gear.imageRes),
+                    contentDescription = gear.nama,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+                IconButton(
+                    onClick = { isFavorite = !isFavorite },
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(8.dp)
+                ) {
+                    Icon(
+                        imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.Favorite,
+                        contentDescription = "Favorite Icon",
+                        tint = if (isFavorite) Color.Red else Color.White
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = gear.nama,
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = gear.deskripsi,
+                style = MaterialTheme.typography.bodyLarge
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Range: Rp ${gear.harga}",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.SemiBold,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Lihat List")
+            }
+        }
+    }
+}
+
+
 
 
 
--- app/src/main/java/model/Source.kt
+++ app/src/main/java/model/Source.kt
@@ -7,4 +7,10 @@ object Source{
         GameGear("Headset", "Rekomendasi Headset Buget", 1000000, R.drawable.headset),
         GameGear("Keyboard", "Rekomendasi Keyboard Buget", 800000, R.drawable.keyboard)
     )
+
+    val reccomendGear = listOf(
+        GameGear(nama = "Pulsar X2", deskripsi = "Approve by Most VCT Masters Winners", harga = 1599000, imageRes = R.drawable.mpulsar),
+        GameGear(nama = "Wooting 60HE", deskripsi = "Approve by Most VCT Master Winner", harga = 1499000, imageRes = R.drawable.kwoot),
+        GameGear(nama = "Hyper X Cloud 3", deskripsi = "Approve by Forsaken", harga = 1200000, imageRes = R.drawable.hhyper )
+    )
 }
